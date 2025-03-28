da8a35403c05757f3f2198ac5aff0dd25111aaad
Ali Raza <129832771+Raza118041@users.noreply.github.com>
2025-03-28 14:48:52 +0500

    add one project

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -4,6 +4,7 @@ import Femclinic from "@/assets/Images/femclinic.png"
 import RubNGub from "@/assets/Images/rubngub.png"
 import CodeSprint from "@/assets/Images/codesprint.png"
 import Tokenty from "@/assets/Images/tokenty.png"
+import ShopNGrab from "@/assets/Images/shopNgrab.png"
 import NavigateImage from "@/assets/navigate_icon.svg"
 import Link from 'next/link'
 
@@ -13,7 +14,7 @@ const Project = () => {
             image: Femclinic,
             projectNumber: "01",
             projectTitle: "Femclinic",
-            projectDescription: "Femclinic is an online healthcare platform designed to simplify the appointment booking process with doctors. The website allows users to browse doctor profiles, check availability, and schedule appointments seamlessly. It features Calendly integration, enabling users to book appointments effortlessly at their preferred time. With a user-friendly interface, Femclinic ensures a smooth and hassle-free experience for both patients and healthcare professionals.",
+            projectDescription: "Femclinic is an online healthcare platform built with Java, Next.js, and MongoDB, designed to simplify the appointment booking process with doctors. The website allows users to browse doctor profiles, check availability, and schedule appointments seamlessly. It features Calendly integration, enabling users to book appointments effortlessly at their preferred time. With a user-friendly interface, Femclinic ensures a smooth and hassle-free experience for both patients and healthcare professionals.",
             navigateImage: NavigateImage,
             href: "https://femclinic.io/"
         },
@@ -21,7 +22,7 @@ const Project = () => {
             image: RubNGub,
             projectNumber: "02",
             projectTitle: "RubNGub",
-            projectDescription: "RubNGub is an eCommerce platform that offers a range of organic products, including ghee, shampoo, oil, and honey. Users can browse high-quality natural products and place orders online with ease. The website ensures a seamless shopping experience with secure payment options and fast delivery. Designed for convenience, RubNGub provides customers with access to pure and chemical-free essentials from the comfort of their homes.",
+            projectDescription: "RubNGub is an eCommerce platform built with NestJS, Next.js, and PostgreSQL, offering a range of organic products, including ghee, shampoo, oil, and honey. Users can browse high-quality natural products and place orders online with ease. The website ensures a seamless shopping experience with secure payment options and fast delivery. Designed for convenience, RubNGub provides customers with access to pure and chemical-free essentials from the comfort of their homes.",
             navigateImage: NavigateImage,
             href: "https://rubngub.com/"
         },
@@ -29,7 +30,7 @@ const Project = () => {
             image: CodeSprint,
             projectNumber: "03",
             projectTitle: "CodeSprint",
-            projectDescription: "CodeSprint is a Lahore-based software company specializing in innovative digital solutions. The company's website showcases its services, including web and mobile app development, custom software solutions, and IT consulting. With a team of skilled professionals, CodeSprint delivers high-quality, scalable, and efficient software products. The website provides insights into the company's expertise, portfolio, and client success stories.",
+            projectDescription: "CodeSprint is a Lahore-based software company specializing in innovative digital solutions. The company's website showcases its services, including web and mobile app development, custom software solutions, and IT consulting. Built using Next.js, Node.js, MongoDB, and Express.js, CodeSprint delivers high-quality, scalable, and efficient software products. The website provides insights into the company's expertise, portfolio, and client success stories.",
             navigateImage: NavigateImage,
             href: "https://codesprint.site/"
         },
@@ -37,7 +38,15 @@ const Project = () => {
             image: Tokenty,
             projectNumber: "04",
             projectTitle: "Tokenty",
-            projectDescription: "Tokenty is a platform for creating and managing smart contracts, enabling users to tokenize assets and automate transactions securely. It allows creators to set up royalty mechanisms, ensuring they earn a percentage from future resales of their digital assets. The website provides tools for seamless blockchain integration, making it easier for users to deploy and manage smart contracts without extensive coding knowledge. Tokenty empowers businesses and individuals to leverage blockchain technology for secure and transparent digital transactions.",
+            projectDescription: "Tokenty is a platform for creating and managing smart contracts, enabling users to tokenize assets and automate transactions securely. It allows creators to set up royalty mechanisms, ensuring they earn a percentage from future resales of their digital assets. Built using Next.js, Node.js, and MongoDB, Tokenty simplifies the process of deploying and managing smart contracts without extensive coding knowledge.",
+            navigateImage: NavigateImage,
+            href: "https://tokenty.vercel.app/"
+        },
+        {
+            image: ShopNGrab,
+            projectNumber: "05",
+            projectTitle: "ShopNGrab",
+            projectDescription: "ShopnGrab is an eCommerce platform offering a wide range of products, including computer accessories, kids' products, and electronics. Built using Next.js, Node.js, Express.js, and MongoDB, it provides a seamless shopping experience with a fast and secure backend. The platform ensures smooth navigation, efficient product management, and a user-friendly interface. With optimized performance and scalability, ShopnGrab delivers a modern online shopping solution.",
             navigateImage: NavigateImage,
             href: "https://tokenty.vercel.app/"
         },
@@ -59,7 +68,7 @@ const Project = () => {
                                 <p className='text-white font-semibold'>{item.projectTitle}</p>
                                 <p className='text-gray-300 break-words text-start overflow-wrap w-full'>{item.projectDescription}</p>
                                 <Link href={item.href}>
-                                    <Image src={item.navigateImage} alt='image' width={15} height={15} className='cursor-pointer' />
+                                    <Image src={item.navigateImage} alt='image' width={18} height={18} className='cursor-pointer mt-2' />
                                 </Link>
                             </div>
                         </div>
